f0d5bcb470b0c5fe6e19f4ad4d1e0129fd76e7c5
derek-byte <derek.s.prog@gmail.com>
2023-10-03 23:23:38 -0400

    Save attendance code on register

--- pages/api/register.js
+++ pages/api/register.js
@@ -8,6 +8,7 @@ export default async function handler(req,res){
     const { email, password } = req.body;
     // Check if user already exists
     const checkUser = await User.findOne({ email: email });
+    const admin = await User.findOne({ email: "sheed9333@wrdsb.ca" });
 
     if (checkUser)
       return res.status(500).json({
@@ -15,7 +16,7 @@ export default async function handler(req,res){
         type: "warning",
       });
 
-    const user = await User.create({...req.body, role: "student", attendance: 0, attendanceCode: " "});
+    const user = await User.create({...req.body, role: "student", attendance: 0, attendanceCode: admin.attendanceCode});
     res.status(200).json({
       message: "User created successfully! 🥳",
       type: "success",
